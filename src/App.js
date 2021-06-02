e62d8dc67314646d5646092a69df3684113dd395
Sergey-Proskurnin <proskurnin@ua.fm>
2021-06-03 00:16:00 +0300

    Add start App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleIncrementGood = () => {
+    this.setState((prevState) => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleIncrementNeutral = () => {
+    this.setState((prevState) => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleIncrementBad = () => {
+    this.setState((prevState) => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    // const { good, neutral, bad } = this.state;
+    return this.state.good
+  }
+  countPositiveFeedbackPercentage() {}
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <button type="button" onClick={this.handleIncrementGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.handleIncrementNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.handleIncrementBad}>
+          Bad
+        </button>
+
+        <h2>Statistics</h2>
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {good+neutral+bad}</p>
+          <p>Positive feedback: {(good+neutral)/(good+neutral+bad)*100}%</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
